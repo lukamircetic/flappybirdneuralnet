af1979e85a090570dfeed3614ef39af46b9fcb7e
Luka Mircetic <49099101+lukamircetic@users.noreply.github.com>
2020-01-08 15:19:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-const TOTAL = 1000;
 var birds = [];
 var savedBirds = [];
 var pipes = [];
 let counter = 0;
 let cycles = 1;
+let TOTAL = 1000;
+let button100;
+let button250;
+let button500;
+let button1000;
 let slider;
 let highscore = 0;
 let ahs = 0;
@@ -33,11 +37,26 @@ function preload() {
 
 }
 function setup() {
-    let canvas = createCanvas(800, 600);
+    let canvas = createCanvas(800, 600); 
     canvas.parent('canvascontainer');
-    //slider = createSlider(1,100,1);
     slider = select('#speedSlider');
     speedSpan = select('#speed');
+    
+    button100 = createButton('100');
+    button100.position(1000,200);
+    button100.mousePressed(buttonone);
+
+    button200 = createButton('200');
+    button200.position(1000, 250);
+    button200.mousePressed(buttontwo);
+
+    button500 = createButton('500');
+    button500.position(1000, 300);
+    button500.mousePressed(buttonthree);
+
+    button1000 = createButton('1000');
+    button1000.position(1000, 350);
+    button1000.mousePressed(buttonfour);
     for (let i = 0; i < TOTAL; i++) {
         birds[i] = new Bird;    
     }
@@ -49,7 +68,6 @@ function setup() {
 function draw() {
     background(0);
     image(bg, bgX, 0, bg.width, height);
-
     let cycles = slider.value();
     speedSpan.html(cycles);
     for (let n = 0; n < cycles; n++) { //
@@ -106,7 +124,55 @@ function draw() {
     ahSpan.html(ahs);
 }
 
-// function keyPressed() {
+function buttonone() {
+    TOTAL = 100;
+    counter = 0;
+    birds=[];
+    for (let i = 0; i<TOTAL;i++) {
+        birds[i] = new Bird;
+    }
+    savedBirds = [];
+    pipes=[];
+    console.log("Button Pressed");
+    console.log(birds);
+}
+function buttontwo() {
+    TOTAL = 200;
+    counter = 0;
+    birds=[];
+    for (let i = 0; i<TOTAL;i++) {
+        birds[i] = new Bird;
+    }
+    savedBirds = [];
+    pipes=[];
+    console.log("Button Pressed");
+    console.log(birds);
+}
+function buttonthree() {
+    TOTAL = 500;
+    counter = 0;
+    birds=[];
+    for (let i = 0; i<TOTAL;i++) {
+        birds[i] = new Bird;
+    }
+    savedBirds = [];
+    pipes=[];
+    console.log("Button Pressed");
+    console.log(birds);
+}
+function buttonfour() {
+    TOTAL = 1000;
+    counter = 0;
+    birds=[];
+    for (let i = 0; i<TOTAL;i++) {
+        birds[i] = new Bird;
+    }
+    savedBirds = [];
+    pipes=[];
+    console.log("Button Pressed");
+    console.log(birds);
+}
+// // function keyPressed() {
 //     if (key == ' ') {
 //         // console.log("space");
 //         birds.up();
